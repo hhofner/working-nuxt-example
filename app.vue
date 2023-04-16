f06ab960140dda6c4e5c0ac421b432ede0cae9c0
Hans Hofner <hhofner@gmail.com>
2023-04-16 21:16:07 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+</template>
+
+<script>
+
+const pages = [
+    "index",
+    "books",
+]
+</script>
+
+<style>
+
+:root {
+
+  /* Typography */
+  --font-size: 16px;
+
+/*  Colors */
+
+/*  Spacing */
+
+}
+
+</style>
